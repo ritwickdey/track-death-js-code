20439054471961f27e9b69d8a2c6ddb08bb0f56b
Ritwick Dey <ritwickdey@outlook.com>
2020-08-09 17:24:00 +0530

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+const { createDeathFileParser } = require("./lib/createDeathFileParser");
+
+const startFilePath = process.argv[2];
+
+const indexFile = (function getIndexFile() {
+  const indexFile = path.resolve(process.cwd(), startFilePath);
+  if (indexFile.endsWith(".js") || indexFile.endsWith(".jsx")) {
+    return indexFile;
+  }
+  return path.join(indexFile, "index.js");
+})();
+
+const deathFileParser = createDeathFileParser();
+const { usedJsFiles, allJsfiles, unusedJsFiles } = deathFileParser.init(
+  indexFile
+);
+
+console.log(
+  `\nUsed JS Files: ${usedJsFiles.length}, ununsed: ${unusedJsFiles.length}, Total:${allJsfiles.length}`
+);
+
+const logPath = path.join(__dirname, "log");
+fs.writeFileSync(
+  path.join(logPath, "usedJsFiles.json"),
+  JSON.stringify(usedJsFiles, null, 2)
+);
+fs.writeFileSync(
+  path.join(logPath, "unusedJsFiles.json"),
+  JSON.stringify(unusedJsFiles, null, 2)
+);
+
+fs.writeFileSync(
+  path.join(logPath, "allJsFiles.json"),
+  JSON.stringify(allJsfiles, null, 2)
+);
+
+console.log(`Logs are generated into ${logPath}`);
